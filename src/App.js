8f1a32ee7dd43bceba814836a8c85dc9c2f934a6
james_dovey <jamesd5190@gmail.com>
2017-03-07 16:56:00 +0000

    expand svg

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
           <div className="logo"><Logo fill="#ff3ccc" /></div>
         </header>
         <div className="map">
+        <a href="#"><img src="/img/expand.svg" width="50px" className="expand" /></a>
           <ReactMapboxGl
             style="mapbox://styles/jamesdovey/ciziu0nhl003r2skfn5vy1u4k" // eslint-disable-line react/style-prop-object
             accessToken="pk.eyJ1IjoiamFtZXNkb3ZleSIsImEiOiJjaXppdHkzNnkwMDQxMzJxcHU2ZDlqc2swIn0.Ure8r4g5Ka4ox2xNFYf5rQ"
