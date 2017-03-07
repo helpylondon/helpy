54530275c726d2cf24c78b88dc4b86905bbb8988
Henrique Alves <henriquea@gmail.com>
2017-03-07 12:37:15 +0000

    Lint file

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
         </header>
         <div className="map">
           <ReactMapboxGl
-            style="mapbox://styles/jamesdovey/ciziu0nhl003r2skfn5vy1u4k"
+            style="mapbox://styles/jamesdovey/ciziu0nhl003r2skfn5vy1u4k" // eslint-disable-line react/style-prop-object
             accessToken="pk.eyJ1IjoiamFtZXNkb3ZleSIsImEiOiJjaXppdHkzNnkwMDQxMzJxcHU2ZDlqc2swIn0.Ure8r4g5Ka4ox2xNFYf5rQ"
             containerStyle={{
-              height: "100vh",
-              width: "100vw"
+              height: '100vh',
+              width: '100vw'
             }}>
           </ReactMapboxGl>
         </div>
