04b49ded44977b635840c581bd42c638abf10fe8
Henrique Alves <henriquea@gmail.com>
2017-03-07 12:33:27 +0000

    Add react mapbox component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import ReactMapboxGl from "react-mapbox-gl";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Helpy
-      </div>
+      <article>
+        <header className="header">
+          Logo
+        </header>
+        <div className="map">
+          <ReactMapboxGl
+            style="mapbox://styles/jamesdovey/ciziu0nhl003r2skfn5vy1u4k"
+            accessToken="pk.eyJ1IjoiamFtZXNkb3ZleSIsImEiOiJjaXppdHkzNnkwMDQxMzJxcHU2ZDlqc2swIn0.Ure8r4g5Ka4ox2xNFYf5rQ"
+            containerStyle={{
+              height: "100vh",
+              width: "100vw"
+            }}>
+          </ReactMapboxGl>
+        </div>
+        <section className="profiles">
+          <div className="profile"></div>
+          <div className="profile"></div>
+          <div className="profile"></div>
+          <div className="profile"></div>
+          <div className="profile"></div>
+        </section>
+      </article>
     );
   }
 }
