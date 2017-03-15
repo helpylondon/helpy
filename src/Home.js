585194d52ffbbc926bba965178907d8994c667c9
Henrique Alves <henriquea@gmail.com>
2017-03-15 14:28:08 +0000

    Add greensock

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import {TweenMax, TimelineMax} from 'gsap';
+
 class Home extends React.Component {
+
+  componentDidMount() {
+    // animate stuff here
+    // this.title DOM
+    TweenMax.from(this.title, 1, { alpha: 0 });
+  }
+
   render() {
     return (
-      <div>
+      <div ref={div => this.title = div}>
         Home
       </div>
     )
