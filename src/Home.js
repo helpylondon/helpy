a7afac2bb73ac021bb9dd420ded3214c7d26e770
Henrique Alves <henriquea@gmail.com>
2017-03-15 23:57:50 +0000

    Cleanup home

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import {TweenMax, TimelineMax, Power2} from 'gsap';
+import {TimelineMax, Power2} from 'gsap';
 
 import './Home.css';
 
 class Home extends React.Component {
 
   componentDidMount() {
-    // animate stuff here
-    // this.title DOM
-    var tl = new TimelineMax({repeat:1}),
-				quote = this.quote,
-				img = this.image,
-        btn = this.btn;
-
-        tl
-    				.from(quote, 1, { x:300, alpha: 0, ease: Power2.easeInOut} )
-    				.from(img, 1, { y:500, alpha: 0, ease: Power2.easeInOut}, "-=0.7" )
-            .from(btn, 1, { alpha:0 })
-
+    const tl = new TimelineMax();
+    tl
+      .from(this.quote, 1, {x: 300, alpha: 0, ease: Power2.easeInOut})
+			.from(this.img, 1, {y: 500, alpha: 0, ease: Power2.easeInOut}, '-=0.7')
+      .from(this.btn, 1, {alpha: 0});
   }
 
   render() {
@@ -31,16 +24,15 @@ class Home extends React.Component {
               Lee has been on the streets now for 2 years, and wants to become a lifeguard.
             </p>
             <div ref={div => this.btn = div}>
-              <Link to="/Help" className="button hvr-grow">Help Now</Link>
+              <Link to="/support" className="button hvr-grow">Help Now</Link>
             </div>
           </div>
           <div className="col-xs-12 col-lg-6">
-            <img src="/img/lee.jpg" width="100%" className="home-img" ref={div => this.image = div} />
+            <img src="/img/lee.jpg" width="100%" className="home-img" ref={div => this.image = div} alt="Lee" />
           </div>
         </div>
       </div>
-
-    )
+    );
   }
 }
 
