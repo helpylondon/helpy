e37fc4c12a0c8b08735e39fa543876167f478121
Henrique Alves <henriquea@gmail.com>
2017-03-15 14:12:05 +0000

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import App from './App';
 import Home from './Home';
